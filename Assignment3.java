bc79d2b84e281a606332695427bbd7cdcff6c834
mbeave <39030128+mbeave@users.noreply.github.com>
2022-05-12 14:21:17 -0500

    initial commit

---
+++ Assignment3.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+/******************************************************************
+   This program searches a given directory for a single-term/permuterm
+   query. It then reports which documents in the directory contain
+   said search query. It ranks the documents based on the TF-IDF 
+   score of each document.
+ ******************************************************************/
+
+public class Assignment3
+{
+  public static void main (String args[]) throws Exception
+  {
+	System.out.println();
+    IRSystem ir = new IRSystem();
+    ir.start();
+  }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# IR-System
+Information retrieval system built for my Information and Knowledge Management class at UT-Tyler
+
+This program searches a given directory of text files for a query given by the user. It then returns the files that contain said query. The documents returned will be ranked by their TF-IDF score.
